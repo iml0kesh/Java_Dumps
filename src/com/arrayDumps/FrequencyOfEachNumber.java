d1194c22a64ec3232a2ca43899afc168bbc7ca08
lokesh matta <iamlokeshofficial@gmail.com>
2023-02-28 18:59:37 +0530

    rest added

---
+++ src/com/arrayDumps/FrequencyOfEachNumber.java
@@ -0,0 +1,24 @@
+package com.arrayDumps;
+
+import java.util.Scanner;
+
+public class FrequencyOfEachNumber {
+    public static void findFrequency(int a[]){
+        int frequency[] = new int[101];
+        for(int i=0 ;i<a.length; i++){
+            frequency[a[i]]++;
+        }
+        for(int x:frequency){
+            System.out.print(x+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int a[] = new int[size];
+        for(int i=0; i<size; i++){
+            a[i] = sc.nextInt();
+        }
+        findFrequency(a);
+    }
+}
