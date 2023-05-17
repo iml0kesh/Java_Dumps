730d409a81c4656f469951f6b82fbc3e0e2ecae7
lokesh matta <iamlokeshofficial@gmail.com>
2023-05-17 16:02:11 +0530

    class file added

---
+++ src/com/singlelinkedListDumps/FindMiddleNode01.java
@@ -0,0 +1,69 @@
+package com.singlelinkedListDumps;
+
+//This is approach one to find the middle element.
+public class FindMiddleNode01 {
+    class Node{
+        int data;
+        Node next;
+        public Node(int x){
+            this.data = x;
+            this.next = null;
+        }
+    }
+    public Node head = null;
+    public Node tail = null;
+
+    public void addNode(int x) {
+        Node newNode = new Node(x);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public int countNodes(Node x){
+        Node temp = x;
+        int count = 0;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public Node findMiddleElement(Node h){
+        if(h == null) return null;
+        if(h.next == null) return h;
+
+        // count the nummber of Nodes
+        int count = countNodes(h);
+//        to print the 1 middle element ||
+//        if(count % 2 == 0){
+//            count = count / 2 - 1;
+//        } else {
+//            count /= 2;
+//        }
+//        to print the secomd middle element.
+        count = count / 2;
+
+        Node temp = h;
+        while(count > 0){
+            temp = temp.next;
+            count--;
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        FindMiddleNode01 fm = new FindMiddleNode01();
+        fm.addNode(3);
+        fm.addNode(2);
+        fm.addNode(10);
+        fm.addNode(9);
+        Node middleNode = fm.findMiddleElement(fm.head);
+        System.out.println(middleNode.data);
+    }
+}
---
+++ src/com/singlelinkedListDumps/FindMiddleNode02.java
@@ -0,0 +1,54 @@
+package com.singlelinkedListDumps;
+
+public class FindMiddleNode02 {
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int x) {
+            this.data = x;
+            this.next = null;
+        }
+    }
+    public Node head = null;
+    public Node tail = null;
+
+    public void addNode(int x){
+        Node newNode = new Node(x);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node findMiddleElement(Node x){
+        if(x == null) return null;
+        Node fast = x;
+        Node slow = x;
+        while(fast != null && fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+//        return slow; // This will return the middle Element for ODD size List and 1st middle Element in EVEN Size List.
+
+        if(fast.next == null){
+            return slow; // ODD case MIDDLE ELEMENT.
+        } else {
+            return slow.next; // EVEN case 2nd Middle.
+        }
+
+
+    }
+    public static void main(String[] args) {
+        FindMiddleNode02 fm = new FindMiddleNode02();
+        fm.addNode(3);
+        fm.addNode(2);
+        fm.addNode(10);
+        fm.addNode(9);
+        Node middleNode = fm.findMiddleElement(fm.head);
+        System.out.println(middleNode.data);
+    }
+}
