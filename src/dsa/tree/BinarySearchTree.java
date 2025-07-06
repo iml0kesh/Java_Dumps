66481994f705dff12670c6944781e302fc6f5b21
Lokesh Matta <iamlokeshofficial@gmail.com>
2025-07-06 22:09:38 +0530

    Java material added with some class files.

--- src/dsa/tree/BinarySearchTree.java
+++ src/dsa/tree/BinarySearchTree.java
@@ -1,6 +1,9 @@
 package dsa.tree;
 
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 class Node {
@@ -39,10 +42,12 @@ public class BinarySearchTree {
         root = addRecursive(root, value);
     }
 
+    // Inorder Traversal
+    // Left -> Root -> Right
     private void traverseInOrder(Node node) {
         if (node != null) {
             traverseInOrder(node.left);
-            System.out.println(" " + node.value);
+            System.out.print(node.value + "_");
             traverseInOrder(node.right);
         }
     }
@@ -51,17 +56,78 @@ public class BinarySearchTree {
         traverseInOrder(root);
     }
 
+    // Preorder Traversal
+    // Root -> Left -> Right
+    private void traversePreOrder(Node node){
+        if(node != null) {
+            System.out.print(node.value + "_");
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    // Postorder Traversal
+    // Left -> Right -> Root
+    private void traversePostOrder(Node node) {
+        if(node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(node.value + "_");
+        }
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void BFS(Node root) {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            currentNode = queue.remove();
+//            list.add(currentNode.value);
+            System.out.print(currentNode.value+" ");
+            if(currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+    }
+
+    public void BFS() {
+        BFS(root);
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BinarySearchTree bst = new BinarySearchTree();
 
-        bst.add(1);
-        bst.add(4);
-        bst.add(0);
-        bst.add(7);
-        bst.add(6);
-        bst.add(7);
+        bst.add(47);
+        bst.add(21);
+        bst.add(76);
+        bst.add(18);
+        bst.add(27);
+        bst.add(52);
+        bst.add(82);
 
         bst.traverseInOrder();
+        System.out.println();
+
+        bst.traversePreOrder();
+        System.out.println();
+
+        bst.traversePostOrder();
+        System.out.println();
+
+        bst.BFS();
     }
 }
